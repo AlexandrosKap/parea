ab48b60de4466d6750c665183de143160a85b9b3
AlexandrosKap <alexandroskapretsos@gmail.com>
2022-05-03 07:24:44 +0300

    Godot is a bug free engine.

--- src/components/PareaTextBox.gd
+++ src/components/PareaTextBox.gd
@@ -1,6 +1,9 @@
 extends VBoxContainer
 
-var text_speed := 16.0
+# Used for appending text to fix a Godot bug...
+var __old_visible_characters := 0
+
+var text_speed := 24.0
 
 onready var label: RichTextLabel = $Panel/Margin/Label
 
@@ -10,6 +13,10 @@ func _ready() -> void:
 
 # Update label.percent_visible until its value is 1.
 func _process(delta: float) -> void:
+	if __old_visible_characters:
+		label.visible_characters = __old_visible_characters
+		__old_visible_characters = 0
+	
 	var char_count := label.get_total_character_count()
 	if char_count:
 		label.percent_visible += (1.0 / char_count) * text_speed * delta
@@ -24,10 +31,13 @@ func show_text(text: String) -> void:
 	label.percent_visible = 0.0
 	set_process(true)
 
-# TODO: Appending does not change label.percent_visible.
 func add_text(text: String) -> void:
 	var err := label.append_bbcode(text)
 	if err:
 		assert(false, "Can't append text.")
 	else:
+		if label.visible_characters == -1:
+			__old_visible_characters = label.get_total_character_count()
+		else:
+			__old_visible_characters = label.visible_characters
 		set_process(true)
